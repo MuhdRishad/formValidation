39cf27fb7e739b55ceb29f84dfb835ff6b1c6417
Muhammed Rishad k <muhammedrishad2000@gmail.com>
2022-09-04 19:28:38 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,82 @@
+function validate(){
+    var firstname = document.myForm.firstname.value;
+    if (firstname == "") {
+        error_fname.innerHTML = "Enter your first name";
+        document.myForm.firstname.focus();
+        return false;
+    } else {
+        error_fname.innerHTML = "";
+    }
+
+
+    var lastname = document.myForm.lastname.value;
+    if (lastname == "") {
+        error_lname.innerHTML = "Enter your last name";
+        document.myForm.lastname.focus();
+        return false;
+    } else {
+        error_lname.innerHTML = "";
+    }
+
+    var username = document.myForm.username.value;
+    if (username == ""){
+        error_uname.innerHTML = "Enter username";
+        document.myForm.username.focus();
+        return false;
+    } else {
+        error_uname.innerHTML = "";
+    }
+
+    var email = document.myForm.email.value;
+    var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    var check = re.test(email);
+    if ( check == false ){
+        error_email.innerHTML = "Enter a valid email";
+        document.myForm.email.focus();
+        return false;
+    } else {
+        error_email.innerHTML = "";
+    }
+
+
+    var gender = document.myForm.gender.value;
+    if ( gender == "" ) {
+        error_gender.innerHTML = "Select gender";
+        document.myForm.gender.focus();
+        return false;
+    } else {
+        error_gender.innerHTML = "";
+    }
+
+
+    var pincode = document.myForm.pincode.value;
+    if ( pincode == "" || isNaN()) {
+        error_pincode.innerHTML = "Enter valid pincode";
+        document.myForm.pincode.focus();
+        return false;
+    } else {
+        error_pincode.innerHTML = "";
+    }
+
+    var password = document.myForm.password.value;
+    var re = /^[A-Za-z0-9]\w{8}$/;
+    var check = re.test(password);
+    if ( check == false ) {
+        error_password.innerHTML = "Enter a password between minimum 8 charecters";
+        document.myForm.password.focus();
+        return false;
+    } else {
+        error_password.innerHTML = ""
+    }
+
+
+    var confirmpass = document.myForm.confirmpass.value;
+    if (password != confirmpass) {
+        error_confirmpass.innerHTML = "Password deos't match !";
+        document.myForm.confirmpass.focus();
+        return false;
+    } else {
+        error_confirmpass.innerHTML = "";
+    }
+    return true;   
+}
